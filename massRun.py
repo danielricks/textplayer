76ef86f4ef42019deeb49f4d33e430133ca4b714
Dan <dan@potts.cs.byu.edu>
2016-05-09 18:49:28 -0600

    Modified where the output is sent.

--- massRun.py
+++ massRun.py
@@ -13,10 +13,11 @@ for game in os.listdir(os.getcwd() + '/games'):
 '''
 
 
-t = TextPlayer()
-t.assign_variables('zork1.z5', False)
-t.run()
-t.parse_and_execute_command_file('commands.txt')
-t.execute_command('look')
+t = TextPlayer('zork1.z5', True)
+start_info = t.run()
+print start_info
+#t.parse_and_execute_command_file('commands.txt')
+command_output = t.execute_command('look')
+print command_output
 t.quit()
 
