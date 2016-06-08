d975bf39fb362ce00443fe14dd8a4af0e50e328d
kingjamesiv <daniel.ricks2@gmail.com>
2016-06-08 12:35:57 -0600

    Updated massRun.py to reflect architectural changes
    It now must be run outside the textplayer folder.

--- massRun.py
+++ massRun.py
@@ -1,29 +1,30 @@
 import os
-from textPlayer import TextPlayer
+import textplayer.textPlayer as tp
 '''
 # For running commands on multiple games
 game_num = 1
-for game in os.listdir(os.getcwd() + '/games'):
-	print '--------------------------------------------------'
-	print game, game_num
-	t = TextPlayer(game, False)
-	start_info = t.run()
-	print start_info
-	command_output = t.execute_command('go west')
-	print command_output
-	command_output = t.execute_command('go east')
-	print command_output
-	command_output = t.execute_command('go east')
-	print command_output
-	command_output = t.execute_command('go east')
-	print command_output
-	command_output = t.execute_command('look')
-	print command_output
-	game_num += 1
+for game in os.listdir(os.getcwd() + '/textplayer/games'):
+        print '--------------------------------------------------'
+        print game_num, game
+        t = tp.TextPlayer(game)
+        start_info = t.run()
+        print start_info
+        command_output = t.execute_command('go west')
+        print command_output
+        command_output = t.execute_command('go east')
+        print command_output
+        command_output = t.execute_command('go east')
+        print command_output
+        command_output = t.execute_command('go east')
+        print command_output
+        command_output = t.execute_command('look')
+        print command_output
+        game_num += 1
+
 '''
 # For running commands on a single game
 
-t = TextPlayer('zork1.z5', True)
+t = tp.TextPlayer('zork1.z5')
 start_info = t.run()
 print start_info
 command_output = t.execute_command('open mailbox')
